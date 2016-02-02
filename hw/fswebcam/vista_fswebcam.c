406e5e64a60012f84e181402e0f149002b4a5284
Mark Carey <mark_carey@mentor.com>
2016-02-02 22:00:16 +0000

    24 bit seems to be OK

--- hw/fswebcam/vista_fswebcam.c
+++ hw/fswebcam/vista_fswebcam.c
@@ -667,7 +667,7 @@ int vista_fswc_grab()
 		}
 	}
 
-	bmp = bmp_create(config->width, config->height, 16);
+	bmp = bmp_create(config->width, config->height, 24);
 	rgb_pixel_t pixel = {128, 64, 0, 0};
 
 	pbitmap = abitmap;
